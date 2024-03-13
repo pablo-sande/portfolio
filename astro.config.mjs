5ab2bb8452950324521f935862ef93b9632243c2
pablo-sande <sandepenapablo@gmail.com>
2024-03-13 19:29:42 +0100

    Trying to fix iOS animation flicker while maintaining fixed header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'hybrid',
-  server: { port: 3000, host: true},
-  adapter: node({
-    mode: 'standalone'
-  })
+  server: {
+    port: 3000,
+    host: true
+  },
+  adapter: vercel()
 });
