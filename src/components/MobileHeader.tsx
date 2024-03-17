0fb0aa09e04852293ec2f1b157a83fe5ceb37a11
pablo-sande <sandepenapablo@gmail.com>
2024-03-17 04:20:33 +0100

    Bump version and removed headlessui

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -1,5 +1,4 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+import { useState } from 'react'
 import '../styles/styles.scss'
 
 export const MobileHeader = () => {
